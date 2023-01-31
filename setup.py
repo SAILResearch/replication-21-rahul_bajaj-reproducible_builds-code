53a12d0cc665d825e635d2f97c82e2dbcc3fa842
Rahul Bajaj <rahulrb0509@gmail.com>
2023-01-31 00:41:50 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='reproducible-builds',
+    version='1.0',
+    py_modules=['reroducible-builds'],
+    install_requires=[
+        'beautifulsoup4',
+        'html-table-parser-python3',
+        'lifelines',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'pandas',
+        'requests',
+        'scipy',
+        'seaborn',
+        'setuptools',
+        'urllib3',
+    ],
+)
+
